10850e38fe33a8d09a04a22efa75afa212c37777
HOKAGO-MEMORIES <minchotejava@gmail.com>
2025-01-03 05:01:01 +0900

    feat: header 구현

---
+++ src/components/header.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import { Home, Moon, Sun } from "lucide-react";
+import { useTheme } from "next-themes";
+import Link from "next/link";
+import Nav from "./nav";
+import { Button } from "./ui/button";
+
+export default function Header() {
+    return (
+        <header className="h-14 px-5 sticky top-0 z-50 w-full border-b flex justify-between items-center bg-background">
+            <div className="flex items-center">
+                <Nav />
+
+            </div>
+            <div className="flex items-center gap-1">
+                <ThemeChanger />
+				<Logo />
+			</div>
+
+        </header>
+    );
+}
+
+function Logo() {
+    return (
+        <Link href="/" className="font-extrabold">
+            <Button
+                type="button"
+                variant="outline"
+                size="icon"
+                className="rounded-full"
+            >
+                <Home className="h-5 w-5 transition-all text-primary stroke-1"/>
+            </Button>
+        </Link>
+    );
+}
+
+function ThemeChanger() {
+	const { setTheme, theme } = useTheme();
+
+	const toggleTheme = () => {
+		setTheme(theme === "dark" ? "light" : "dark");
+	};
+
+	return (
+		<Button
+			type="button"
+			variant="outline"
+			size="icon"
+			onClick={toggleTheme}
+			className="rounded-full"
+		>
+			<Sun className="h-5 w-5 transition-all dark:hidden text-primary stroke-1" />
+			<Moon className="h-5 w-5 hidden transition-all dark:block text-primary stroke-1" />
+			<span className="sr-only">Toggle theme</span>
+		</Button>
+	);
+}
---
+++ src/components/nav.tsx
@@ -0,0 +1,60 @@
+"use client"
+
+import { ArrowUpRight } from "lucide-react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { cn } from "./ui/utils";
+
+interface Nav {
+    href: string;
+    label: string;
+    external?: boolean;
+}
+
+export const navs = [
+    {
+        href: "/blog",
+        label: "Blog",
+    },
+    {
+        href: "/project",
+        label: "Project",
+    },
+];
+
+
+export default function Nav() {
+    return (
+        <nav className="sm:flex hidden items-center space-x-6 text-sm font-medium gap-2">
+            {navs.map((nav) => (
+                <NavItem key={nav.label} {...nav}/>
+            ))}
+
+        </nav>
+
+    );
+}
+
+export function NavItem({
+    href,
+    label,
+    external,
+    onClick,
+}: Nav & { onClick?: () => void }) {
+    const pathname = usePathname();
+
+    return (
+        <Link
+            href={href}
+            className={cn(
+                "transition-colors hover:font-black py-3 text-primary font-bold flex items-center",
+                pathname?.startsWith(href) && "underline underline-offset-4 font-black",
+            )}  
+            target={external ? "_blank" : undefined}     
+            onClick={onClick}
+        >
+            {label.toUpperCase()}
+            {external && <ArrowUpRight className="h-4 w-4" />}
+        </Link>
+    )
+}
