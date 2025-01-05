b9c1835d902ffb575cd8461764f47cafc6e46f3f
HOKAGO-MEMORIES <minchotejava@gmail.com>
2025-01-05 22:15:16 +0900

    fix: footer 수정정

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,8 +4,8 @@ import Link from "next/link"
 
 export default function Footer() {
     return (
-        <footer className="flex flex-col items-center justyfy-center w-full h-28 gap-3">
-            <div className="flex items-center justyfy-center gap-3">
+        <footer className="flex flex-col items-center justify-center w-full h-28 gap-3">
+            <div className="flex items-center justify-center gap-3">
                 <Link href="https://github.com/HOKAGO-MEMORIES" target="_blank">
                     <GithubIcon size={20} className="text-primary stroke-1"/>
                 </Link>
