d3aa91c8a194995ee774631cf3fa6bd162ee6b74
HOKAGO-MEMORIES <minchotejava@gmail.com>
2025-01-08 23:17:46 +0900

    feat: 프로젝트 항목 추가

--- src/app/projects/data/projects.ts
+++ src/app/projects/data/projects.ts
@@ -15,4 +15,19 @@ export const projects: Project[] = [
         description: "백준 알고리즘 문제 풀이 모음집",
         link: "https://github.com/HOKAGO-MEMORIES/BOJ-line123",
     },
+    {
+        title: "The Way Home",
+        description: "대학 캡스톤 디자인 출품작 - 2D 플랫폼 게임",
+        link: "https://github.com/HOKAGO-MEMORIES/The-Way-Home-Scripts",
+    },
+    {
+        title: "디스코드 가챠 봇",
+        description: "제비 뽑기, 팀 짜기를 지원하는 디스코드 봇",
+        link: "https://github.com/HOKAGO-MEMORIES/Discord-Gacha-Bot",
+    },
+    {
+        title: "깃허브 블로그",
+        description: "(구) 블로그",
+        link: "https://github.com/HOKAGO-MEMORIES/hokago-memories.github.io",
+    },
 ];
