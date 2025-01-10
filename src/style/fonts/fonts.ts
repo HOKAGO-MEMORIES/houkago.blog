35aaea5c3bab8b8388988ae6ddea939835aacbf5
HOKAGO-MEMORIES <minchotejava@gmail.com>
2025-01-11 04:00:06 +0900

    feat: 폰트를 "나눔스퀘어 네오"로 변경

---
+++ src/style/fonts/fonts.ts
@@ -0,0 +1,9 @@
+import localFont from "next/font/local";
+
+const nanumSquareNeo = localFont({
+    src: "./NanumSquareNeo-Variable.woff2",
+    display: "swap",
+});
+
+export { nanumSquareNeo };
+  
