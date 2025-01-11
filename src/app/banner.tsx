584457ea96826f7e86bb2b06eb9cfc93612ab8b4
HOKAGO-MEMORIES <minchotejava@gmail.com>
2025-01-12 03:33:52 +0900

    feat: 메인 이미지 변경

--- src/app/banner.tsx
+++ src/app/banner.tsx
@@ -1,11 +1,11 @@
 export default function Banner() {
     return (
-        <div className="block w-full h-[800px] p-6 bg-no-repeat bg-center bg-[url('/home/james_webb.png')]">
-            <h1 className="text-white text-5xl font-semibold text-primary break-all">
-                YONGHWI KIM
+        <div className="block w-full aspect-[16/9] p-6 bg-cover bg-no-repeat  bg-center bg-[url('/home/main.jpg')]">
+            <h1 className="text-5xl font-semibold text-primary break-all">
+                방과후 블로그
             </h1>
-            <p className="text-white text-xl break-all mt-20">
-                BLOG
+            <p className="text-xl break-all text-muted mt-4">
+                YONGHWI KIM
             </p>
         </div>
     );
