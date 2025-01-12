dc11f4540cea90d8a878bd29b2279e7f13183c20
HOKAGO-MEMORIES <minchotejava@gmail.com>
2025-01-12 19:40:27 +0900

    feat: 최신 포스트에서 ps 포스트도 출력

--- src/app/components/recent-posts.tsx
+++ src/app/components/recent-posts.tsx
@@ -1,31 +1,33 @@
-import { blogPosts } from "#posts";
+import { blogPosts, psPosts } from "#posts";
 import Link from "next/link";
 
 export default function RecentPosts() {
+    const allPosts = [...blogPosts, ...psPosts].sort(
+        (a, b) => Number(new Date(b.date)) - Number(new Date(a.date))
+    )
+
     return (
         <>
             <h2 className="mt-20 text-5xl font-extrabold text-primary">
                 Recent Posts
             </h2>
             <div className="flex flex-col">
-                {blogPosts
-                    .sort((a, b) => Number(new Date(b.date)) - Number(new Date(a.date)))
+                {allPosts
                     .slice(0, 5)
-                    .map((blog) => (
+                    .map((post) => (
                         <Link
-                            href={`${blog.permalink}`}
-                            key={blog.slug}
+                            href={`${post.permalink}`}
+                            key={post.slug}
                             className="flex py-5 gap-2 flex-1 border-b last:border-none"
                         >
                             <div className="flex flex-col">
                                 <span className="font-bold text-lg break-keep text-primary">
-                                    {blog.title}
+                                    {post.title}
                                 </span>
-                                <span className="break-all">{blog.desc}</span>
+                                <span className="break-all">{post.desc}</span>
                             </div>
-                            <time className="text-xs ml-auto">{blog.date}</time>
+                            <time className="text-xs ml-auto">{post.date}</time>
                         </Link>
-                        
                 ))}
             </div>
         </>
