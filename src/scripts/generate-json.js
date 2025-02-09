3a0be50023e5c56934bb863d5e0467de81013ace
HOKAGO-MEMORIES <minchotejava@gmail.com>
2025-02-09 22:40:23 +0900

    feat: 파일을 다운로드 받는 GET 메서드 추가

--- src/scripts/generate-json.js
+++ src/scripts/generate-json.js
@@ -1,19 +1,55 @@
-import 'dotenv/config'
-import { exec } from 'child_process';
+import 'dotenv/config';
+import fs from 'fs';
+import path from 'path';
 
-const url = process.env.API_URL;
-const blogRoot = process.cwd().replace(/\\/g, "/");
-const jsonBody = (JSON.stringify({ blogRoot })).replace(/"/g, '\\"');
+const API_URL = process.env.API_URL;
+const POSTS_DIR = path.join(process.cwd(), '.posts');
 
-const curlCommand = `curl -X POST "${url}" -H "Content-Type: application/json" -d "${jsonBody}"`;
+function makePostsDirectory() {
+    if (!fs.existsSync(POSTS_DIR))
+        fs.mkdirSync(POSTS_DIR);
+}
 
-exec(curlCommand, (error, stdout, stderr) => {
-    if (error) {
-        console.error(`ERROR: ${error.message}`);
-        console.error(`stderr: ${stderr}`);
-        return;
+async function fetchGeneratedFileNames() {
+    const response = await fetch(`${API_URL}/api/generate-json`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' }
+    });
+
+    if (!response.ok)
+        throw new Error(`❌ Failed to generate files: ${response.statusText}`);
+
+    return response.json();
+}
+
+async function downloadFile(fileName) {
+    const fileResponse = await fetch(`${API_URL}/api/files/${fileName}`);
+
+    if (!fileResponse.ok)
+        throw new Error(`❌ Failed to download ${fileName}`);
+
+    const arrayBuffer = await fileResponse.arrayBuffer();
+    const buffer = Buffer.from(arrayBuffer);
+    fs.writeFileSync(path.join(POSTS_DIR, fileName), buffer);
+    console.log(`✅ Downloaded: ${fileName}`);
+}
+
+async function generateJson() {
+    try {
+        makePostsDirectory();
+
+        const fileNames = await fetchGeneratedFileNames();
+        
+        for (const name of fileNames) {
+            await downloadFile(name);
+        }
+
+        console.log(`✅ All files generated and downloaded successfully`)
+
+    } catch (error) {
+        console.error('Error:', error.message);
+        process.exit(1);
     }
+}
 
-    console.log('✅ Generated JSON files successfully');
-    console.log(stdout);
-});
+generateJson();
